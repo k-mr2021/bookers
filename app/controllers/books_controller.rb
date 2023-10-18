1ff48a4a63e0c99ac38efa0f36c32572e61e48b7
k-mr2021 <143944891+k-mr2021@users.noreply.github.com>
2023-10-18 15:37:39 +0000

    [Finish] 課題2..2日目

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,13 @@
 class BooksController < ApplicationController
   def new
+    # Viewへ渡すためのインスタンス変数に空のオブジェクトを生成する
+    @book = Book.new
+  end
+  
+  def create
+    book = Book.new(book_params)
+    book.save
+    redirect_to '/'
   end
 
   def index
@@ -10,4 +18,12 @@ class BooksController < ApplicationController
 
   def edit
   end
+  
+  private
+  # ストロングメーター
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+  
 end
+

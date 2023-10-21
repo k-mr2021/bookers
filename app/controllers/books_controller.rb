08c77055d5228f0702b059a87a99f0ea586c6409
k-mr2021 <143944891+k-mr2021@users.noreply.github.com>
2023-10-21 06:42:41 +0000

    [Interruption] 課題2..中断

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,25 +1,43 @@
 class BooksController < ApplicationController
   
    def new
-    @book = Book.new
+   
    end 
-
-   def create
-    book = Book.new(book_params)
-    book.save
-    redirect_to book_path(book.id)
-   end
    
    def index
-     @books = Book.all
+    @books = Book.all
+    @book = Book.new
+   end
+   
+
+   def create
+    @book = Book.new(book_params)
+    if @book.save
+      redirect_to book_path(@book.id)
+    else
+      @books = Book.all
+      render :index
+    end
    end
    
    def show
-     @book = Book.find(params[:id])
+    @book = Book.find(params[:id])
    end
   
   def edit
     @book = Book.find(params[:id])
+  end
+  
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book.id)
+  end
+  
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
   end
 
   

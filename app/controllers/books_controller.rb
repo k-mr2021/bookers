7716387b899d2f9110d6280120dc32a152715db3
k-mr2021 <143944891+k-mr2021@users.noreply.github.com>
2023-10-21 15:53:32 +0000

    [Finish] 課題2..4日目

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -26,9 +26,12 @@ class BooksController < ApplicationController
   end
   
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book.id)
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+       redirect_to book_path(@book.id)
+    else
+      render :edit
+    end
   end
   
   def destroy
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+[
+    {name: "リーダブルコード", note: "より良いコードを書くためのシンプルで実践的なテクニック"}, 
+    {name: "トラブル知らずのシステム設計", note: "システム設計の要点を図解で説明してくれます"}, 
+    {name: "たのしいRuby", note: "Rubyの入門におすすめです"}, 
+    {name: "入門Git", note: "gitについての基本操作から内部構造まで解説してくれます"}, 
+    {name: "アルゴリズム図鑑", note: "アルゴリズムについてカラーイラストでしっかり解説してくれます"}, 
+    {name: "達人プログラマー", note: "プログラマ入門者は必読！"}, 
+    {name: "データベース実践入門", note: "	効率的なSQL文を教えてくれます"}, 
+].map{|row| Hashie: :Mash.new(row) }.each do |date|
+    Book.create!(
+      name: date.name,
+      note: date.note,
+      )
+end

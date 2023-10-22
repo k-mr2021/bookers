b78c26be22faa346248cc60c946530145e57cac9
k-mr2021 <143944891+k-mr2021@users.noreply.github.com>
2023-10-22 01:37:44 +0000

    [Start] 課題2..5日目

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -10,6 +10,7 @@ class BooksController < ApplicationController
    def create
     @book = Book.new(book_params)
     if @book.save
+      flash[:notice] = "Book was successfully created."
       redirect_to book_path(@book.id)
     else
       @books = Book.all
@@ -28,6 +29,7 @@ class BooksController < ApplicationController
   def update
     @book = Book.find(params[:id])
     if @book.update(book_params)
+      flash[:notice] = "Book was successfully updated."
        redirect_to book_path(@book.id)
     else
       render :edit
